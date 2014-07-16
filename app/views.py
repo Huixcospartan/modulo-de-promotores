ab1dd02133e2eec97eadba779aa08817e65d7f54
AlonsoCampos <hectorcamposalonso@yahoo.es>
2014-07-15 20:01:14 -0500

    Modulos Agregados
    Detalles Eventos, Editar Cuenta de Usuario

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import TemplateView,FormView
-from django.shortcuts import render_to_response,get_object_or_404
+from django.shortcuts import render_to_response,get_object_or_404, render 
 from django.template.context import RequestContext
 from forms import * 
+from django.views.generic.detail import DetailView
 from django.http import HttpResponse,HttpResponseRedirect, HttpResponseForbidden
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
@@ -28,16 +29,6 @@ def newuser(request):
     else:
         formulario = UserForm()
     return render_to_response(template,locals(), context_instance=RequestContext(request))
-    
-
-@login_required
-def evento(request):    
-    #import ipdb; ipdb.set_trace()
-    evento = Evento.objects.all()
-    #promotor = User.objects.get(id=request.user.id)
-    #evento = get_object_or_404(Evento,pk =2 ) #User.objects.get(id=request.user.id).id)    
-    template = "eventos.html"
-    return render_to_response(template,locals())
 
 @login_required
 def micuenta(request):
@@ -48,6 +39,27 @@ def micuenta(request):
     return render_to_response(template,locals())
 
 @login_required
+def editar_micuenta(request):
+    #import ipdb; ipdb.set_trace()
+    usuario  = User.objects.get(username=request.user.username)
+    promotor = Promotor.objects.get(usuario=usuario)
+    form = UserForm(request.POST, instance=usuario)
+    if form.is_valid():
+        form.save(commit=False)
+        promotor.nombre = form.cleaned_data['nombre']
+        promotor.nombre     = form.cleaned_data['nombre']
+        promotor.paterno    = form.cleaned_data['paterno']
+        promotor.materno    = form.cleaned_data['materno']
+        form.save()
+        promotor.save()
+        return HttpResponseRedirect("/micuenta")   
+    else:
+        form = UserForm()
+
+    template = "editarcuenta.html"
+    return render_to_response(template,context_instance=RequestContext(request,locals()))
+
+@login_required
 def miseventos(request):
     #import ipdb; ipdb.set_trace()
     promotor = Promotor.objects.get(usuario=request.user.id)
@@ -55,10 +67,53 @@ def miseventos(request):
     template = "miseventos.html"
     return render_to_response(template,locals())
 
-# http://blog.byjakt.com/multiple-django-forms-in-one-form.html
-# http://stackoverflow.com/questions/5857363/using-multiple-forms-on-a-page-in-django
-# Buscar en
-#https://www.google.com.mx/search?noj=1&q=django+formview+multiple+forms&revid=538944700&sa=X&ei=XQTCU-zfNuSF8AHLn4GACw&ved=0CIQBENUCKAQ4Cg&biw=1366&bih=657
+@login_required
+def evento(request):    
+    #import ipdb; ipdb.set_trace()
+    promotor = Promotor.objects.get(usuario=request.user.id)
+    evento   = Evento.objects.filter(Promotor=promotor)
+    template = "eventos.html"
+    return render_to_response(template,locals())
+
+@login_required
+def eliminar_evento(request,pk):    
+    #import ipdb; ipdb.set_trace()
+    evento = get_object_or_404(Evento,pk=pk)
+    if request.method=='GET':
+        evento.delete()
+        return HttpResponseRedirect('/miseventos')
+    template = "detallesevento.html"
+    return render_to_response(template,context_instance=RequestContext(request,locals()))
+
+class EventosDetallesView(DetailView):
+    model = Evento
+    context_object_name = 'evento'
+    
+    def get_template_names(self):
+        return 'detallesevento.html'
+
+@login_required
+def actualizar_evento(request,pk):
+    import ipdb; ipdb.set_trace()
+    usuario  = User.objects.get(username=request.user.username)
+    promotor = Promotor.objects.get(usuario=request.user.id)
+    evento      = get_object_or_404(Evento,pk=pk)
+    formulario  = EventoForm(request.POST,request.FILES,prefix="sch", instance=evento)
+    ubicacion = DestinoForm(request.POST,prefix="loc")
+        
+    if formulario.is_valid() and ubicacion.is_valid():
+        destino = ubicacion.save()
+        evento = formulario.save(commit = False)
+        evento.destino = destino
+        evento.Promotor = promotor
+        evento.save()
+        return HttpResponseRedirect("/miseventos")   
+    else:
+        formulario = EventoForm()
+
+    template = "detallesevento.html"
+    return render_to_response(template,context_instance=RequestContext(request,locals()))
+
 
 @login_required
 def add(request):
@@ -85,3 +140,11 @@ def add(request):
     template = "form.html"
     return render_to_response(template,context_instance=RequestContext(request,locals()))
 
+
+
+
+
+# http://blog.byjakt.com/multiple-django-forms-in-one-form.html
+# http://stackoverflow.com/questions/5857363/using-multiple-forms-on-a-page-in-django
+# Buscar en
+#https://www.google.com.mx/search?noj=1&q=django+formview+multiple+forms&revid=538944700&sa=X&ei=XQTCU-zfNuSF8AHLn4GACw&ved=0CIQBENUCKAQ4Cg&biw=1366&bih=657
--- pulse/urls.py
+++ pulse/urls.py
@@ -5,25 +5,35 @@ from django.contrib import admin
 admin.autodiscover()
 from django.middleware.csrf import CsrfViewMiddleware
 
+from app.views import EventosDetallesView
+
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^$','django.contrib.auth.views.login',
-        {'template_name':'inicio.html'} ,name = 'login'),
-
-    url(r'^cerrar/$' , 'django.contrib.auth.views.logout_then_login',
-         name='logout'),
-    url(r'^newuser/$', 'app.views.newuser', name='newuser'),
-    url(r'^evento/$', 'app.views.evento', name='evento'),
-    url(r'^micuenta/$', 'app.views.micuenta', name='micuenta'),
-    url(r'^miseventos/$', 'app.views.miseventos', name='miseventos'),
-    url(r'^add/$', 'app.views.add', name='add'),
-    # url(r'^pulse/', include('pulse.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    
+    # Para el panel de Aministracion
     url(r'^admin/', include(admin.site.urls)),
+
+    #Estas URL son para el login
+    url(r'^$','django.contrib.auth.views.login',{'template_name':'inicio.html'} ,name = 'login'),
+    url(r'^cerrar/$' , 'django.contrib.auth.views.logout_then_login', name='logout'),
+    
+    #Para administrar mis eventos
+    url(r'^miseventos/$', 'app.views.miseventos', name='miseventos'),
+    url(r'^evento/$', 'app.views.evento', name='evento'),
+    url(r'^add/$', 'app.views.add', name='add'),
+    
+    url(r'^eventosdetalle/(?P<pk>[\d]+)',EventosDetallesView.as_view()),
+    url(r'^eventoseliminar/(?P<pk>[\d]+)','app.views.eliminar_evento' , name='evento-delete',),
+    url(r'^eventosactualizar/(?P<pk>[\d]+)','app.views.actualizar_evento' , name='evento-update',),
+    
+    # Accede a registrar un usuario
+    url(r'^newuser/$', 'app.views.newuser', name='newuser'),  
+   
+    # Para editar mi cuenta de usuario
+    url(r'^micuenta/$', 'app.views.micuenta', name='micuenta'),
+    url(r'^editarmicuenta/$', 'app.views.editar_micuenta', name='editar_micuenta'),
+    
+
+    
 )
 urlpatterns += patterns('',
     url(r'^media/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.MEDIA_ROOT}),
